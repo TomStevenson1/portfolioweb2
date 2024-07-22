163ddb86a2e837b63c7643fef9a78de7715f9102
Tom Stevenson <tomjon4340@gmail.com>
2024-07-22 10:56:37 -0500

    Updated skills section layout and styling

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
+import MachineLearning from "./MachineLearning";
 import "./skills.css";
-import Frontend from './Frontend';
-import Backend from './Backend';
 
 const Skills = () => {
-  return (
-    <section className="skills section" id="skills">
-        <h2 className="section__title">Skills</h2>
-        <span className="section__subtitle">With personal ranking</span>
-        <div className="skills__container container grid">
-            <Frontend />
-            <Backend /> 
-        </div>
-    </section>
-  );
+    return (
+        <section className="skills section" id="skills">
+            <h2 className="section__title">Skills</h2>
+            <span className="section__subtitle">My Technical Level</span>
+
+            <div className="skills__container container grid">
+                <Frontend />
+                <Backend />
+                <MachineLearning />
+            </div>
+        </section>
+    );
 }
 
 export default Skills;
