1aa021ec5158aeb0b3c3051a364e16bef84b3a1c
Tom Stevenson <tomjon4340@gmail.com>
2024-07-23 11:43:24 -0500

    Updated package.json and updated header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,69 +1,61 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import './header.css';
-import { HiOutlineHome, HiOutlineUser, HiOutlineBadgeCheck, HiOutlineClipboardList, HiOutlinePhotograph, HiOutlineMail, HiX, HiOutlineMenu } from "react-icons/hi";
-
+import { Link } from 'react-scroll';
 
 const Header = () => {
-    window.addEventListener("scroll", function () {
-        const header = document.querySelector(".header");
-        if(this.scrollY >= 80) header.classList.add("scroll-header");
-        else header.classList.remove("scroll-header");
-    });
-   
-    const[Toggle, showMenu] = useState(false);
-    const[activeNav, setActiveNav] = useState("#home");
+  const [toggle, setToggle] = useState(false);
 
-    return(
-        <header className="header">
-            <nav className="nav container">
-                <a href="index.html" className="nav__logo">Portfolio</a>
-                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#home" onClick={() => setActiveNav("#home")} 
-                            className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
-                                <HiOutlineHome className="nav__icon"/>Home
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#about" onClick={() => setActiveNav("#about")} 
-                            className={activeNav === "#about" ? "nav__link active-link" : "nav__link"}>
-                                <HiOutlineUser className="nav__icon"/>About
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#skills" onClick={() => setActiveNav("#skills")} 
-                            className={activeNav === "#skills" ? "nav__link active-link" : "nav__link"}>
-                                <HiOutlineBadgeCheck className="nav__icon"/>Skills
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#services" onClick={() => setActiveNav("#services")} 
-                            className={activeNav === "#services" ? "nav__link active-link" : "nav__link"}>
-                                <HiOutlineClipboardList className="nav__icon"/>Services
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#portfolio" onClick={() => setActiveNav("#portfolio")} 
-                            className={activeNav === "#portfolio" ? "nav__link active-link" : "nav__link"}>
-                                <HiOutlinePhotograph className="nav__icon"/>Projects
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#contact" onClick={() => setActiveNav("#contact")} 
-                            className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"}>
-                                <HiOutlineMail className="nav__icon"/>Contact
-                            </a>
-                        </li>
-                    </ul>
-                    <HiX className="nav__close" onClick={() => showMenu(!Toggle)} />
-                </div>
-                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-                    <HiOutlineMenu />
-                </div>
-            </nav>
-        </header>
-    )
-}
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-export default Header
+  return (
+    <header className={`header ${toggle ? 'show-menu' : ''}`}>
+      <nav className="nav container">
+        <Link to="home" className="nav__logo">
+          Tom Stevenson
+        </Link>
+        <div className={`nav__menu ${toggle ? 'show-menu' : ''}`}>
+          <ul className="nav__list">
+            <li className="nav__item">
+              <Link to="home" className="nav__link" activeClass="active-link" spy={true} smooth={true} offset={-70} duration={500} onClick={handleToggle}>
+                <i className="nav__icon"></i> Home
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="about" className="nav__link" activeClass="active-link" spy={true} smooth={true} offset={-70} duration={500} onClick={handleToggle}>
+                <i className="nav__icon"></i> About
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="skills" className="nav__link" activeClass="active-link" spy={true} smooth={true} offset={-70} duration={500} onClick={handleToggle}>
+                <i className="nav__icon"></i> Skills
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="qualification" className="nav__link" activeClass="active-link" spy={true} smooth={true} offset={-70} duration={500} onClick={handleToggle}>
+                <i className="nav__icon"></i> Qualification
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="portfolio" className="nav__link" activeClass="active-link" spy={true} smooth={true} offset={-70} duration={500} onClick={handleToggle}>
+                <i className="nav__icon"></i> Portfolio
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="contact" className="nav__link" activeClass="active-link" spy={true} smooth={true} offset={-70} duration={500} onClick={handleToggle}>
+                <i className="nav__icon"></i> Contact
+              </Link>
+            </li>
+          </ul>
+          <i className="nav__close" onClick={handleToggle}></i>
+        </div>
+        <div className="nav__toggle" onClick={handleToggle}>
+          <i className="nav__icon"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
